0e9f73b535445334b1635716f0534378ba445bbd
aroojshafi01 <106294451+aroojshafi01@users.noreply.github.com>
2022-08-21 12:33:05 +0530

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class loop
+{
+public static void main(String ar[])
+{
+int num1;
+System.out.println("Enter number :");
+Scanner Sc=new Scanner(System.in);
+num1=Sc.nextInt();
+System.out.println("Numbers are");
+for(int i=num1;i<200;i++)
+{System.out.println(i);}
+}
+}
